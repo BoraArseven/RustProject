d7af4e675d6128b0ab9c7fa8b31a1a330f09a6df
Bora <bora.arseven@gmail.com>
2024-02-12 18:28:18 +0100

    Configured logfile_parser as reading each term, which is closer to be ready to be used.

--- src/logfile_parser.rs
+++ src/logfile_parser.rs
@@ -17,14 +17,21 @@ pub fn read(path: &str) -> std::io::Result<()> {
         let line = line_result?;
         let mut start = 0;
         let mut end = 0;
-
+        // in this format, the first white space is dividing the timestamp into 2. I don't want it so I ignore the first space with that.
+        // maybe I could merge them together later, so I could pay this for computation later. But it is just a boolean check. Otherwise it will be string merge which is more costly.
+        let mut ignorefirstwhitespace :bool =  true;
         for (i, c) in line.char_indices() {
             if c.is_whitespace() {
+                if ignorefirstwhitespace == false {
+
                 if start < end {
                     let term = &line[start..end];
-                    println!("Term: {}", term);
+                    print!("{} ", term);
                 }
+
+
                 start = i + 1;
+                }else { ignorefirstwhitespace = false }
             }
             end = i + 1;
         }

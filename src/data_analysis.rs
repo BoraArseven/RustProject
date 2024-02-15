f8f9e82d7166fc449f35eea0c0e513b8d0d8a636
Bora <bora.arseven@gmail.com>
2024-02-15 14:39:34 +0100

    added placeholder of print_all_logs function to the data_analysis.rs

--- src/data_analysis.rs
+++ src/data_analysis.rs
@@ -1,13 +1,9 @@
 use crate::logfile_parser::Log;
 // I might be able to use multithreading since our references are not mutable since we are not reading.
-pub(crate) fn request_summary(logs: &Vec<Log>){
+pub(crate) fn request_summary(logs: &Vec<Log>) {}
 
-}
+pub(crate) fn errors(logs: &Vec<Log>) {}
 
-pub(crate) fn errors(logs: &Vec<Log>){
+pub(crate) fn performance(logs: &Vec<Log>) {}
 
-}
-
-pub(crate) fn performance(logs: &Vec<Log>){
-
-}
+pub(crate) fn print_all_logs(logs: &Vec<Log>) {}
